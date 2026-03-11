22bf70a6d6c01ced692f2da610acedfd7adcb5d1
fywww4 <a0900518266@gmail.com>
2026-03-11 16:51:08 +0800

    定期存檔

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator anim;
+    SpriteRenderer sprite;
+    BoxCollider2D coll;
+    [SerializeField] LayerMask jumpableGround;
 
     float dirX = 0f;
     [SerializeField] float moveSpeed = 7f;
     [SerializeField] float jumpForce = 14f;
+
+    enum MovementState { idle, running, jumping, falling}
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGround())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -31,17 +38,40 @@ public class PlayerMovement : MonoBehaviour
 
     void UpdateAnimationState()
     {
+        MovementState state;
+
         if (dirX > 0f)
         {
-            anim.SetBool("Running", true);
+            //anim.SetBool("Running", true);
+            state = MovementState.running;
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("Running", true);
+            //anim.SetBool("Running", true);
+            state = MovementState.running;
+            sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("Running", false);
+            //anim.SetBool("Running", false);
+            state = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("state",(int)state);
+
+    }
+
+    private bool IsGround()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
